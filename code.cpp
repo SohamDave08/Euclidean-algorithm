25670bc962e539be2b1611fa21f0a3849dacfb34
Soham Dave <dave.soham2000@gmail.com>
2020-10-26 11:47:31 +0530

    Final algorithm

---
+++ code.cpp
@@ -0,0 +1,20 @@
+#include "iostream"
+using namespace std;
+
+int GCD(int a, int b)
+{
+    if(a==0)
+        return b;
+    
+    return GCD(b%a,a);
+}
+
+int main()
+{
+    int a,b;
+    cout<<"Enter 2 numbers you want to find GCD for:\n";
+    cin>>a>>b;
+    int result = GCD(a,b);
+    cout<<"\nGCD("<<a<<","<<b<<") = "<<result<<endl;
+    return 0;
+}
